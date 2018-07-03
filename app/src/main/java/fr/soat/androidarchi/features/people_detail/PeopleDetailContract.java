71c58611c4e75e19ab5fcc03e19398a472be9271
Daouda Bangoura <bangouradaouda@gmail.com>
2018-07-03 21:41:44 +0200

    update peopledetail contract

--- app/src/main/java/fr/soat/androidarchi/features/people_detail/PeopleDetailContract.java
+++ app/src/main/java/fr/soat/androidarchi/features/people_detail/PeopleDetailContract.java
@@ -4,18 +4,18 @@ import fr.soat.androidarchi.data.model.People;
 import fr.soat.androidarchi.features.base.BasePresenter;
 import fr.soat.androidarchi.features.base.BaseView;
 
-public class PeopleDetailContract {
+public interface PeopleDetailContract {
 
-    public interface View extends BaseView {
+    interface View extends BaseView {
         void updatePeopleDetail(People people);
         void displayErrorMessage();
     }
 
-    public interface Presenter extends BasePresenter<View> {
+    interface Presenter extends BasePresenter<View> {
         void getPeopleDetail(int peopleId);
     }
 
-    public interface Interactor {
+    interface Interactor {
         void getPeopleWithId(int id, GetPeopleDetailCallback callback);
         interface GetPeopleDetailCallback {
             void onPeopleDetailSuccess(People people);
