366e59bf6d0f8e527afb47b12ec2811dfd642276
Daouda Bangoura <bangouradaouda@gmail.com>
2018-07-03 21:33:45 +0200

    tp1 bases

--- app/src/main/java/fr/soat/androidarchi/data/repository/network/PeopleApi.java
+++ app/src/main/java/fr/soat/androidarchi/data/repository/network/PeopleApi.java
@@ -1,8 +1,10 @@
 package fr.soat.androidarchi.data.repository.network;
 
+import fr.soat.androidarchi.data.model.People;
 import fr.soat.androidarchi.data.model.PeopleListResult;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface PeopleApi {
@@ -10,4 +12,6 @@ public interface PeopleApi {
     @GET("people/")
     Call<PeopleListResult> getPeopleListAtPage(@Query("page") int page);
 
+    @GET("people/{peopleId}")
+    Call<People> getPeopleWithId(@Path("peopleId") Integer id);
 }
