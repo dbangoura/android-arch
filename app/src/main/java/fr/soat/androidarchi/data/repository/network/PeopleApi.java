57828d1a2c2e2a9a7f9a39b6c49ea38f9dd2a284
Daouda Bangoura <bangouradaouda@gmail.com>
2018-07-04 09:14:56 +0200

    update peopleapi

--- app/src/main/java/fr/soat/androidarchi/data/repository/network/PeopleApi.java
+++ app/src/main/java/fr/soat/androidarchi/data/repository/network/PeopleApi.java
@@ -1,10 +1,8 @@
 package fr.soat.androidarchi.data.repository.network;
 
-import fr.soat.androidarchi.data.model.People;
 import fr.soat.androidarchi.data.model.PeopleListResult;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface PeopleApi {
@@ -12,6 +10,4 @@ public interface PeopleApi {
     @GET("people/")
     Call<PeopleListResult> getPeopleListAtPage(@Query("page") int page);
 
-    @GET("people/{peopleId}")
-    Call<People> getPeopleWithId(@Path("peopleId") Integer id);
 }
